e975a3008aac95d8efd35632f9e91686c32405b9
Michael Jarvis <mjfullstack@gmail.com>
2022-06-05 00:03:33 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -14,12 +14,11 @@ setup(
     description='Check your Regexes in Python!',
     long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/nihilok/domains-api',
+    url='https://github.com/nihilok/re_check',
     author='Michael Jarvis',
     author_email='mjfullstack@gmail.com',
     classifiers=classifiers,
     keywords='',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['requests']
 )
